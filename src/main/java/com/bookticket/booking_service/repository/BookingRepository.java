df0be6234338779456aeb32848901d83867ead51
Amit Yadav <akydv2627@gmail.com>
2025-11-14 13:50:45 +0530

    Add Booking Repo and Service

--- src/main/java/com/bookticket/booking_service/repository/BookingRepository.java
+++ src/main/java/com/bookticket/booking_service/repository/BookingRepository.java
@@ -1,4 +1,7 @@
 package com.bookticket.booking_service.repository;
 
-public interface BookingRepository {
+import com.bookticket.booking_service.entity.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookingRepository extends JpaRepository<Booking, Long> {
 }
--- src/main/java/com/bookticket/booking_service/service/BookingService.java
+++ src/main/java/com/bookticket/booking_service/service/BookingService.java
@@ -1,4 +1,28 @@
 package com.bookticket.booking_service.service;
 
+import com.bookticket.booking_service.dto.CreateBookingRequest;
+import com.bookticket.booking_service.entity.Booking;
+import com.bookticket.booking_service.enums.BookingStatus;
+import com.bookticket.booking_service.repository.BookingRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
 public class BookingService {
+    private final BookingRepository bookingRepository;
+
+    public BookingService(BookingRepository bookingRepository) {
+        this.bookingRepository = bookingRepository;
+    }
+
+    public void createBooking(Long userId,CreateBookingRequest createBookingRequest) {
+        log.info("Creating.... Booking For user Id: {}", userId);
+        Booking booking = new Booking();
+        booking.setUserId(userId);
+        booking.setShowId(createBookingRequest.showId());
+        booking.setTotalAmount(100.0);
+        booking.setStatus(BookingStatus.PENDING);
+        Booking createdBooking = bookingRepository.save(booking);
+    }
 }
