24a0c20991f5d3bdef06fb1461da85ed4c5a350d
Amit Yadav <akydv2627@gmail.com>
2025-11-16 22:03:49 +0530

    Add Payment Polling Properties

--- src/main/java/com/bookticket/booking_service/BookingServiceApplication.java
+++ src/main/java/com/bookticket/booking_service/BookingServiceApplication.java
@@ -1,12 +1,18 @@
 package com.bookticket.booking_service;
 
+import com.bookticket.booking_service.configuration.PaymentPollingProperties;
 import com.bookticket.booking_service.configuration.RedisLockProperties;
+import com.bookticket.booking_service.configuration.ServiceUrlProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(RedisLockProperties.class)
+@EnableConfigurationProperties({
+        RedisLockProperties.class,
+        ServiceUrlProperties.class,
+        PaymentPollingProperties.class}
+)
 public class BookingServiceApplication {
 
 	public static void main(String[] args) {
